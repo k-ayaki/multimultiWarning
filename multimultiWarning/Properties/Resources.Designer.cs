2444cf6675eaa955b86507a7d072b6ac9a599400
k-ayaki <hfh00576@nifty.com>
2023-01-28 00:24:31 +0900

    ver.1.1.06

--- multimultiWarning/Properties/Resources.Designer.cs
+++ multimultiWarning/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace multimultiWarning.Properties {
     // または Visual Studio のようなツールを使用して自動生成されました。
     // メンバーを追加または削除するには、.ResX ファイルを編集して、/str オプションと共に
     // ResGen を実行し直すか、または VS プロジェクトをビルドし直します。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -98,7 +98,7 @@ namespace multimultiWarning.Properties {
         }
         
         /// <summary>
-        ///   Ver.1.1.0 に類似しているローカライズされた文字列を検索します。
+        ///   Ver.1.1.05 に類似しているローカライズされた文字列を検索します。
         /// </summary>
         internal static string Version {
             get {
